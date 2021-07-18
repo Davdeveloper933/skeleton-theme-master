f1c2c4b2ddc9fc4f8e17c2acbdb4ef21788f4ca3
David Istomin <daveistomin@gmail.com>
2021-07-18 18:35:03 +0400

    fixing cart popup functionality

--- assets/theme.js
+++ assets/theme.js
@@ -1,57 +1,22 @@
-const mobMenu = document.getElementById('mobileMenu');
-const hamburger = document.getElementById('hamburger');
-const expandedMenu = document.getElementById('expandedMenu');
-const addCartBtn = document.getElementById('addToCart');
+const mobMenuIcon = document.getElementById('mobileMenuIcon');
+const mobileMenu = document.querySelector('.mobile__menu');
+const mobileMenuCloseBtn = document.getElementById('expandedMenu');
 const prodForm = document.getElementById('productForm');
-const prod_select = document.getElementById('productSelect');
 const prodPrice = document.querySelector('.product__price__value');
-let prod_id = prod_select.value;
+const cart_total_price = document.querySelector('.cart__subtotal__number');
+const item_count_num = document.querySelector('.item__count__val');
+// console.log(document.querySelectorAll('.cart__item__title'))
 
+let getTheCartData = {};
 
-prod_select.addEventListener('change',() =>{
-    prod_id = prod_select.value;
-    prodPrice.textContent = prod_select.options[prod_select.selectedIndex].dataset.variant_price;
-    console.log(prod_select.options[prod_select.selectedIndex].dataset.variant_price)
+mobMenuIcon.addEventListener('click',() => {
+    mobileMenu.classList.remove('closed');
+    mobileMenu.classList.add('open')
 })
 
-console.log(prod_id)
-let formData = {
-    'items': [{
-        'id': 36110175633573,
-        'quantity': 1
-    }]
-};
-
-addCartBtn.addEventListener('click', async () => {
-    // e.preventDefault();
-
-    let response = await fetch('/cart/add.js', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'items': [
-                {
-                    'id': prod_id,
-                    'quantity': 1
-                }
-            ]
-        })
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-)
-
-
-mobMenu.addEventListener('click',() => {
-    hamburger.classList.toggle('hide')
-    expandedMenu.classList.toggle('hide')
+mobileMenuCloseBtn.addEventListener('click',() => {
+    mobileMenu.classList.remove('open');
+    mobileMenu.classList.add('closed')
 })
 
 const swiper = new Swiper(".mySwiper", {
@@ -77,3 +42,54 @@ const swiper2 = new Swiper(".mySwiper2", {
         swiper: swiper,
     }
 })
+
+var Shopify = Shopify || {};
+// ---------------------------------------------------------------------------
+// Money format handler
+// ---------------------------------------------------------------------------
+Shopify.money_format = "${{amount}}";
+Shopify.formatMoney = function(cents, format) {
+    if (typeof cents == 'string') { cents = cents.replace('.',''); }
+    var value = '';
+    var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
+    var formatString = (format || this.money_format);
+
+    function defaultOption(opt, def) {
+        return (typeof opt == 'undefined' ? def : opt);
+    }
+
+    function formatWithDelimiters(number, precision, thousands, decimal) {
+        precision = defaultOption(precision, 2);
+        thousands = defaultOption(thousands, ',');
+        decimal   = defaultOption(decimal, '.');
+
+        if (isNaN(number) || number == null) { return 0; }
+
+        number = (number/100.0).toFixed(precision);
+
+        var parts   = number.split('.'),
+            dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+            cents   = parts[1] ? (decimal + parts[1]) : '';
+
+        return dollars + cents;
+    }
+
+    switch(formatString.match(placeholderRegex)[1]) {
+        case 'amount':
+            value = formatWithDelimiters(cents, 2);
+            break;
+        case 'amount_no_decimals':
+            value = formatWithDelimiters(cents, 0);
+            break;
+        case 'amount_with_comma_separator':
+            value = formatWithDelimiters(cents, 2, '.', ',');
+            break;
+        case 'amount_no_decimals_with_comma_separator':
+            value = formatWithDelimiters(cents, 0, '.', ',');
+            break;
+    }
+
+    return formatString.replace(placeholderRegex, value);
+};
+
+console.log(Shopify.formatMoney(145, Shopify.money_format))
